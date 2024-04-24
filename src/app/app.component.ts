e283fef3806e49f9a48dbaceb30cd85a2353e780
Jongita <jurgitajonaityte@gmail.com>
2024-04-24 11:14:45 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddProductComponent } from './componets/add-product/add-product.component';
+import { ListProductsComponent } from './componets/list-products/list-products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddProductComponent, ListProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
